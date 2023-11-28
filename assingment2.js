5073e05925410b8cc71781b5c84da1c8a555536d
Yeabsiramichael5 <126076921+Yeabsiramichael5@users.noreply.github.com>
2023-11-28 09:10:35 +0300

    Add files via upload
    react assignment 2

---
+++ assingment2.js
@@ -0,0 +1,11 @@
+function acceptnumber(){
+      let n=prompt("enter the number")
+       if(n>0){
+        for(let i=1;i<=n;i+=2){
+       console.log(i) 
+    }
+       }
+       else
+       prompt("invalid")
+}
+acceptnumber();
